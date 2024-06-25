ac486fc1cbc81cb8bf09fa229f338c2f60b04c42
gonglowbat <gong.panupong@gmail.com>
2024-06-25 18:39:42 +0700

    Refactor

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,26 +1,27 @@
 import { Grid as GridDrei } from '@react-three/drei'
-import { useControls } from 'leva'
 import { configs } from '../enums/configs'
+import { useMemo } from 'react'
 
 const Grid = () => {
-    const { gridSize, ...gridConfig } = useControls({
-        gridSize: [configs.width, configs.height],
-        cellSize: { value: 1, min: 0, max: 10, step: 0.1 },
-        cellThickness: { value: 0.6, min: 0, max: 5, step: 0.1 },
+    const position = useMemo(() => [-0.5, -0.51, -0.5], [])
+    const size = useMemo(() => [configs.width, configs.height], [])
+    const config = useMemo(() => ({
+        cellSize: 1,
+        cellThickness: 0.6,
         cellColor: '#6f6f6f',
-        sectionSize: { value: 4, min: 0, max: 10, step: 0.1 },
-        sectionThickness: { value: 1, min: 0, max: 5, step: 0.1 },
+        sectionSize: 4,
+        sectionThickness: 1,
         sectionColor: '#9d4b4b',
-        fadeDistance: { value: 100, min: 0, max: 100, step: 1 },
-        fadeStrength: { value: 0, min: 0, max: 1, step: 0.1 },
+        fadeDistance: 100,
+        fadeStrength: 0,
         followCamera: false,
         infiniteGrid: false,
-    })
+    }), [])
 
     return (
         <>
-            <GridDrei position={[-0.5, -0.51, -0.5]} args={gridSize} {...gridConfig} />
-            <GridDrei position={[-0.5, -0.51, -0.5]} args={gridSize} {...gridConfig} rotation-z={Math.PI} />
+            <GridDrei position={position} args={size} {...config} />
+            <GridDrei position={position} args={size} {...config} rotation-z={Math.PI} />
         </>
     )
 }
